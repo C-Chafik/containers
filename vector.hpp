61c92c83af5c9d2fedd556b127dba8df83ae7b5f
Chafik <qatar75020@gmail.com>
2022-06-25 21:00:49 +0200

    rien

--- vector.hpp
+++ vector.hpp
@@ -6,13 +6,26 @@
 /*   By: cmarouf <cmarouf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 15:49:52 by cmarouf           #+#    #+#             */
-/*   Updated: 2022/06/24 15:50:07 by cmarouf          ###   ########.fr       */
+/*   Updated: 2022/06/25 18:03:07 by cmarouf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VECTOR_HPP
 # define VECTOR_HPP
 
+# include <memory>
 
+namespace ft
+{
+    template< class T, class allocator = std::allocator<T> >
+    class   vector
+    {
+        typedef T                               value_type;
+        typedef allocator                       allocator_type;
+        typedef 
+
+        explicit vector();
+    };
+}
 
 #endif
